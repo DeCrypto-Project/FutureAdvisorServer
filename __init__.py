defa6e03f4c1fd99aecc6355a1f3965b763466d7
Nadav Porat <71311119+NadavPorat@users.noreply.github.com>
2022-08-06 14:13:55 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,43 @@
+import flask_restful
+import redis as redis
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask import Flask
+
+from flask_apispec import FlaskApiSpec
+from flask_cors import CORS
+
+from server.api.Gini import Gini
+
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on server
+api = flask_restful.Api(app)
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='futre ',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0',
+
+
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+})
+cache = redis.Redis(host='redis', port=6379)
+api.add_resource(Gini, '/Gini')
+docs = FlaskApiSpec(app)
+docs.register(Gini)
+
+
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Expose-Headers', 'Content-Type,Content-Length,Authorization,X-Pagination')
+    return response
+
