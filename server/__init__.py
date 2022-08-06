f75bbfec60e5b4c28bb675af10c34659a5a23aab
nporat <nadavpo@mta.ac.il>
2022-08-06 22:31:46 +0300

    make run is working now

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,8 @@
+from flask import Flask
 import flask_restful
 import redis as redis
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
-from flask import Flask
-
 from flask_apispec import FlaskApiSpec
 from flask_cors import CORS
 
@@ -12,15 +11,15 @@ from server.api.Gini import Gini
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
-app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on server
+app.config[
+    'PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on server
 api = flask_restful.Api(app)
 app.config.update({
     'APISPEC_SPEC': APISpec(
-        title='futre ',
+        title='Future Advisor Server ',
         version='v1',
         plugins=[MarshmallowPlugin()],
         openapi_version='2.0.0',
-
 
     ),
     'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
@@ -32,7 +31,6 @@ docs = FlaskApiSpec(app)
 docs.register(Gini)
 
 
-
 @app.after_request
 def add_headers(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -40,4 +38,3 @@ def add_headers(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Expose-Headers', 'Content-Type,Content-Length,Authorization,X-Pagination')
     return response
-
